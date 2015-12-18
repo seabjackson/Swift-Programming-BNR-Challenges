58fe7c54fcd42b4bb90adad74960775b12f8d30a
Seab Jackson <honeyboyseab@gmail.com>
2015-12-18 13:57:25 +0800

    added challenge 1 solution

---
+++ 1 Getting Started/LastName.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: Create a New Instance of the String type, and set the value of this instance to your last name. Prints its value to the console.
+
+import Cocoa
+
+// print*() function is used to print messages to the console.
+
+var lastName = "Jackson"
+print(lastName)
